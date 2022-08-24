afd56cb27c4038fb6430483e471c03650a487ab3
Kazi Abdur Rakib <57065763+karakib2k18@users.noreply.github.com>
2022-08-25 02:51:08 +0600

    15.ZigZag tree.cpp updated

--- Data Structures and Algorithms/Lecture 12 : Binary Trees/15.ZigZag tree.cpp
+++ Data Structures and Algorithms/Lecture 12 : Binary Trees/15.ZigZag tree.cpp
@@ -37,13 +37,77 @@ class BinaryTreeNode {
 };
 
 using namespace std;
+
 /**********************************************************
 Time complexity: O(N) 
 Space complexity: O(H) 
 where N is the number of nodes in the input tree and 
 H is the height of the input tree
 ***********************************************************/
+//---------------------using 2 STACK Technique---------------------------
+#include <stack>
+void zigZagOrder(BinaryTreeNode<int> *root)
+{
+	stack<BinaryTreeNode<int> *> s1;
+	stack<BinaryTreeNode<int> *> s2;
+	s1.push(root);
+	int currentLevelRemaining = 1;
+	int nextLevelCount = 0;
+	bool flag = true;
+	while (!s1.empty() || !s2.empty())
+	{
+		if (flag)
+		{
+			BinaryTreeNode<int> *top = s1.top();
+			s1.pop();
+			cout << top->data << " ";
+			currentLevelRemaining--;
+			if (top->left != NULL)
+			{
+				s2.push(top->left);
+				nextLevelCount++;
+			}
+			if (top->right != NULL)
+			{
+				s2.push(top->right);
+				nextLevelCount++;
+			}
+			if (currentLevelRemaining == 0)
+			{
+				cout << "\n";
+				currentLevelRemaining = nextLevelCount;
+				nextLevelCount = 0;
+				flag = false;
+			}
+		}
+		else
+		{
+			BinaryTreeNode<int> *top = s2.top();
+			s2.pop();
+			cout << top->data << " ";
+			currentLevelRemaining--;
+			if (top->right != NULL)
+			{
+				s1.push(top->right);
+				nextLevelCount++;
+			}
+			if (top->left != NULL)
+			{
+				s1.push(top->left);
+				nextLevelCount++;
+			}
+			if (currentLevelRemaining == 0)
+			{
+				cout << "\n";
+				currentLevelRemaining = nextLevelCount;
+				nextLevelCount = 0;
+				flag = true;
+			}
+		}
+	}
+}
 
+//---------------------using Vector and divided by 2 Technique---------------------------
 //https://www.youtube.com/watch?v=3OXWEdlIGl4
 #include <vector>
 void zigZagOrder(BinaryTreeNode<int> *root)
